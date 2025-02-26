1f1bf2261dd7dbeaeff5ed799da393e8e5ad0f39
kei0710KEI <keisuke0710.taka@gmail.com>
2025-02-26 11:00:37 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Hero from './components/custom/Hero'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
